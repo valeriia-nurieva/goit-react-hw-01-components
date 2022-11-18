bcd99fc29ff4d90726905ff052ed14f1e81d3b25
valeriia-nurieva <valerka.0906@gmail.com>
2022-11-18 17:39:06 +0200

    add components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import user from '../user.json';
+import data from '../data.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        avatar={user.avatar}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      />{' '}
+      <Statistics title="Upload stats" stats={data} />
+    </>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+
+export default function Statistics({ stats, title }) {
+  return (
+    <section class="statistics">
+      <h2 class="title">{title}</h2>
+      <ul class="stat-list">
+        {stats.map(stat => {
+          return (
+            <li class="item" key={stat.id}>
+              <span class="label">{stat.label}</span>
+              <span class="percentage">{stat.percentage}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+  })).isRequired, 
+};
